1daf089877c4b5cadb9d1ba07a661442d37645d4
DigitalMrPrince <139235016+Digbk@users.noreply.github.com>
2025-02-24 18:57:09 +0530

    Add files via upload

---
+++ Storing birthdays(ACP).py
@@ -0,0 +1,19 @@
+FatherD = 7
+FatherM = "october"
+
+print("birthdate of dad is: " , FatherD, FatherM)
+
+MotherD = 21
+MotherM = "may"
+
+print("birthdate of mom is: " , MotherD, MotherM)
+
+HoneyD = 14
+HoneyM = "feb"
+
+print("birthdate of sister is: " , HoneyD, HoneyM)
+
+PrinceD = 7
+PrinceM = "october"
+
+print("birthdate of Prince is: " , PrinceD, PrinceM)
